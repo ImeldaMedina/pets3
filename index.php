4aed539eda85030a165b5f9db4bb8f49172c89c9
Maureen Kariuki <mkariuki2@mail.greenriver.edu>
2020-01-31 13:46:43 -0500

    Made changes in views and index.php

--- index.php
+++ index.php
@@ -10,6 +10,12 @@ require("vendor/autoload.php");
 //Instantiate F3
 $f3 = Base::Instance();
 
+//set debug level
+$f3->set('DEBUG', 3);
+
+//define an array of colors
+$f3->set('colors', array('pink', 'green', 'blue'));
+
 //Define a default route
 $f3->route("GET /", function (){
     echo "<h1>My Pets</h1>";
@@ -39,12 +45,12 @@ $f3->route("GET /@animal", function($f3, $params) {
     }
 });
 
-$f3->route("GET /order", function() {
+$f3->route("GET|POST /order", function() {
     $views = new Template();
     echo $views->render('views/form1.html');
 });
 
-$f3->route("POST /order2", function() {
+$f3->route("GET|POST /order2", function() {
     //var_dump($_POST);
     $_SESSION['animal'] = $_POST['animal'];
     //var_dump($_SESSION);
